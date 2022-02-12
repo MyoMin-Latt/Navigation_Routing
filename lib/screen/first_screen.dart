d2431685566c22c9f2da43c3ec344e1af086ce95
MyoMinLatt <myominlatt330@gmail.com>
2022-02-12 11:03:26 +0630

    @13.9 Navigation Animation Part 3 [Fade & Scale Transition]

--- lib/screen/first_screen.dart
+++ lib/screen/first_screen.dart
@@ -14,34 +14,69 @@ class FirstScreen extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
-            ElevatedButton(onPressed: (){
-              Navigator.push(context, PageRouteBuilder(
-                transitionDuration: Duration(seconds: 3),
-                pageBuilder: (_, animation, __){
-                  return SlideTransition(
-                    child: SecondScreen(),
-                    position: Tween<Offset>(
-                      begin: Offset(1,0),
-                      end: Offset(0,0)
-                    ).animate(animation));
-                }));
-            }, child: Text("Slide Navigator")),
             ElevatedButton(
                 onPressed: () {
                   Navigator.push(
                       context,
                       PageRouteBuilder(
-                        transitionDuration: Duration(seconds: 2),
-                        pageBuilder: (_, animation, __){
-                          return RotationTransition(
-                            child: SecondScreen(),
-                            turns: Tween<double>(
-                              begin: 0.5,
-                              end: 1,
-                            ).animate(CurvedAnimation(parent: animation, curve: Curves.bounceInOut)));
-                        }));
+                          transitionDuration: Duration(seconds: 3),
+                          pageBuilder: (_, animation, __) {
+                            return SlideTransition(
+                                child: SecondScreen(),
+                                position: Tween<Offset>(
+                                        begin: Offset(1, 0), end: Offset(0, 0))
+                                    .animate(CurvedAnimation(
+                                        parent: animation,
+                                        curve: Curves.bounceInOut)));
+                          }));
                 },
-                child: const Text("Go to Second Screen")),
+                child: Text("Slide Navigator")),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                          transitionDuration: Duration(seconds: 2),
+                          pageBuilder: (_, animation, __) {
+                            return RotationTransition(
+                                child: SecondScreen(),
+                                turns: Tween<double>(
+                                  begin: 0.5,
+                                  end: 1,
+                                ).animate(CurvedAnimation(
+                                    parent: animation,
+                                    curve: Curves.bounceInOut)));
+                          }));
+                },
+                child: const Text("Rotation Navigate")),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                          transitionDuration: Duration(seconds: 3),
+                          pageBuilder: (_, animation, __) {
+                            return FadeTransition(
+                                child: SecondScreen(),
+                                opacity: Tween<double>(begin: 0.0, end: 1)
+                                    .animate(animation));
+                          }));
+                },
+                child: Text('Fade Navigate')),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                          transitionDuration: Duration(seconds: 3),
+                          pageBuilder: (_, animation, __) {
+                            return ScaleTransition(
+                                child: SecondScreen(),
+                                scale: Tween<double>(begin: 0, end: 1)
+                                    .animate(animation));
+                          }));
+                },
+                child: Text('Scale Navigate'))
           ],
         ),
       ),
